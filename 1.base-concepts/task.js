a0121dd3a9f503cb9aedcc1fa2dac688699fafe5
Gorenko Maria <kirillovsub@yandex.ru>
2025-04-16 11:27:27 +0300

    Задание 2 "Функции"

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,15 +2,15 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
-  if (d < 0){
+  if (d < 0) {
     return arr;
   }
-  if (d === 0){
+  if (d === 0) {
     let x = -b / (2 * a);
     arr.push(x);
     return arr;
   }
-  if (d > 0){
+  if (d > 0) {
     let x1 = (-b + Math.sqrt(d)) / (2 * a);
     let x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
